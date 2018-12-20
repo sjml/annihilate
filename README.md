0f11016746cd32279f66ac983ec105fe8215d374
Shane Liesegang <shane@techie.net>
2018-12-20 10:54:27 -0500

    annihilate

---
+++ README.md
@@ -0,0 +1,5 @@
+# Annihilate
+
+_[Annihilation](http://www.imdb.com/title/tt2798920/)_ is a really good movie. You should watch it. 
+
+[This site](https://shaneliesegang.com/annihilate/) will play [the noise](https://slate.com/culture/2018/02/annihilation-co-composer-ben-salisbury-explains-the-musical-cue-from-the-alien.html) at random intervals. Just leave the tab open and go about your day. You can set the upper and lower bounds of how often it will happen. No, you can’t see how long until the next one. No, you can’t stop it. (You can close the tab or reload the page, though.)
---
+++ annihilate.js
@@ -0,0 +1,84 @@
+var ctrls = null;
+var spinner = null;
+var aud = null;
+var btn = null;
+var interval = null;
+var initial = null;
+var min = null;
+var max = null;
+
+var waiting = null;
+
+function play(e) {
+  if (waiting) {
+    clearTimeout(waiting);
+    waiting = null;
+  }
+  var buttonPress = false;
+  if (e && e.srcElement == btn) {
+    buttonPress = true;
+    btn.innerHTML = "restart"
+  }
+
+  var lower = Number(min.value);
+  var upper = Number(max.value);
+
+  if (interval.value == 0) {
+  }
+  else if (interval.value == 1) {
+    lower *= 60;
+    upper *= 60;
+  }
+  else if (interval.value == 2) {
+    lower *= 3600;
+    upper *= 3600;
+  }
+  else if (interval.value == 3) {
+    lower *= 86400;
+    upper *= 86400;
+  }
+
+  // account for duration of audio clip
+  lower += aud.duration;
+  upper += aud.duration;
+
+  var next = Math.random() * (upper - lower) + lower;
+  console.log(next);
+  waiting = setTimeout(play, next * 1000);
+  if (buttonPress && !initial.checked) {
+    return Promise.resolve();
+  }
+  else {
+    return aud.play();
+  }
+}
+
+function setup() {
+  ctrls = document.getElementsByClassName("controls")[0];
+  spinner = document.getElementsByClassName("spinner")[0];
+  aud = document.getElementById("creep");
+  btn = document.getElementById("play");
+  interval = document.getElementById("interval");
+  initial = document.getElementById("initial");
+  min = document.getElementById("min");
+  max = document.getElementById("max");
+
+  function reveal() {
+    ctrls.classList.remove("gone");
+    spinner.classList.add("gone");
+    btn.onclick = function(e) {
+      play(e);
+    };
+  }
+
+  if (aud.readyState < 4) {
+    aud.oncanplaythrough = function() {
+      reveal();
+    };
+  }
+  else {
+    reveal();
+  }
+
+}
+
